4375ffa7dcc8b02a3b5b107c7a056c9876f94122
David Kindl <david.kindl@aira.cz>
2026-04-29 17:05:43 +0200

    Fix issue with HP flickering (0x11 sending full hp while 0xA1 sending percentual)

--- src/network/send.cpp
+++ src/network/send.cpp
@@ -180,8 +180,8 @@ PacketObjectStatus::PacketObjectStatus(const CClient* target, CObjBase* object) 
 		if ( objectChar )
 		{
             fCanRename = objectChar->IsOwnedBy(character);
-			iHitsCurrent = (word)objectChar->Stat_GetVal(STAT_STR);
-            iHitsMax = (word)objectChar->Stat_GetMaxAdjusted(STAT_STR);
+            const ushort tmpMaxHits = objectChar->Stat_GetMaxAdjusted(STAT_STR);
+		    iHitsCurrent = (word)((objectChar->Stat_GetVal(STAT_STR) * 100) / maximum(tmpMaxHits, 1));
 		}
 		else
 		{
